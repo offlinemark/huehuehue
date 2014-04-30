511eb1ab932e98ae5f854e57b05ca5b2b3cc54b0
Mark Mossberg <mark.mossberg@gmail.com>
2014-04-30 00:05:12 -0400

    add fields for bridge ip and user
    - also, once a user has entered in those values, they the form is
      prepopulated with them until the page is exited

--- huehuehue.py
+++ huehuehue.py
@@ -2,14 +2,16 @@
 
 from flask import Flask, render_template, request
 import pyhue
-import config as CFG
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
-        bridge = pyhue.Bridge(CFG.IP, CFG.USER)
+        ip = request.form['ip']
+        user = request.form['user']
+
+        bridge = pyhue.Bridge(ip, user)
 
         if 'off' in request.form.keys():
             for light in bridge.lights:
@@ -17,6 +19,8 @@ def home():
         if 'on' in request.form.keys():
             for light in bridge.lights:
                 light.on = True
+
+        return render_template('index.html', ip=ip, user=user)
 
     return render_template('index.html')
 
